e041b280b33101f4caa4799484bdcebe942ecdd0
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-11-08 13:56:36 +0100

    Add people feature - reducer and sagas

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -4,6 +4,9 @@ import {
   FETCH_FILMS_STARTED,
   FETCH_FILMS_SUCCEEDED,
   FETCH_FILMS_FAILED,
+  FETCH_PEOPLE_STARTED,
+  FETCH_PEOPLE_SUCCEEDED,
+  FETCH_PEOPLE_FAILED,
 } from '../actions';
 
 type Category =
@@ -26,7 +29,10 @@ export type Action = {
   type:
     | typeof FETCH_FILMS_STARTED
     | typeof FETCH_FILMS_SUCCEEDED
-    | typeof FETCH_FILMS_FAILED;
+    | typeof FETCH_FILMS_FAILED
+    | typeof FETCH_PEOPLE_STARTED
+    | typeof FETCH_PEOPLE_SUCCEEDED
+    | typeof FETCH_PEOPLE_FAILED;
   status: string;
   isLoading: boolean;
   payload: {
