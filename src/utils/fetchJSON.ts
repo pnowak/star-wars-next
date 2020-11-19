6a8db42c01b7cbde03c9330142549a45c4177c2a
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-11-19 20:48:17 +0100

    Remove src/ directory

--- src/utils/fetchJSON.ts
+++
@@ -1,8 +0,0 @@
-export const fetchJSON = async (
-  input: string
-): Promise<Record<string, unknown>> => {
-  const res = await fetch(input);
-  const data = await res.json();
-
-  return data;
-};
