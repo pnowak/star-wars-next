05f09f5374143cba7c6b6f1a1ed7daa698e79b96
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-11-27 20:30:06 +0100

    Add root reducer

---
+++ rootReducer.ts
@@ -0,0 +1,18 @@
+import { combineReducers } from 'redux';
+import { filmsReducer } from './features/films/filmsReducer';
+import { peopleReducer } from './features/people/peopleReducer';
+import { planetsReducer } from './features/planets/planetsReducer';
+import { speciesReducer } from './features/species/speciesReducer';
+import { starshipsReducer } from './features/starships/starshipsReducer';
+import { vehiclesReducer } from './features/vehicles/vehiclesReducer';
+
+export const rootReducer = combineReducers({
+  films: filmsReducer,
+  people: peopleReducer,
+  planets: planetsReducer,
+  species: speciesReducer,
+  starships: starshipsReducer,
+  vehicles: vehiclesReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
